4c8965dfa92527c0139e240c24cf792617c42d12
Robert Holm <Robert.Holm@valpo.edu>
2021-01-14 14:15:31 -0600

    Added test for the controller Get method

---
+++ OrganizationAPITests/OrganizationPhoneControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using OrganizationAPI.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OrganizationAPITests
+{
+	public class OrganizationPhoneControllerTests
+	{
+		Logger<OrganizationAPI.Controllers.OrganizationPhoneController> _logger;
+		IConfiguration _configuration;
+
+		[SetUp]
+		public void Setup()
+		{
+			_logger = new Logger<OrganizationAPI.Controllers.OrganizationPhoneController>(new LoggerFactory());
+			_configuration = new ConfigurationBuilder().AddJsonFile("appSettings.Json").Build();
+		}
+
+		[Test]
+		public void GetOrganizationsTest()
+		{
+			OrganizationAPI.Controllers.OrganizationPhoneController controller = new OrganizationAPI.Controllers.OrganizationPhoneController(_logger, _configuration);
+			Task<string> organizationSummary = controller.GetAsync();
+			organizationSummary.Wait();
+
+			Assert.IsTrue(!string.IsNullOrEmpty(organizationSummary.Result));
+		}
+	}
+}
